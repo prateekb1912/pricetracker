09d26ebba842589c21f61078df6e224fcdf1ed45
prateekb1912 <prateekb1912@gmail.com>
2022-11-02 18:47:59 +0530

    Display data scraped from products page

--- pricetracker/tracker/models.py
+++ pricetracker/tracker/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Product(models.Model):
+    asin = models.CharField(max_length=20, unique = True, default='ASIN000')
     title = models.CharField(max_length=256)
     url = models.URLField(max_length=512)
-    slug = models.SlugField()
     list_price = models.DecimalField(max_digits=10, decimal_places=2)
     sell_price = models.DecimalField(max_digits=10, decimal_places=2)
     date_added = models.DateField(auto_now_add=True)
--- pricetracker/tracker/views.py
+++ pricetracker/tracker/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 import requests
+import json
 from bs4 import BeautifulSoup
 
 # Create your views here.
@@ -17,16 +19,26 @@ def get_product_details(url):
     title = soup.find('h1', attrs={'id':'title'}).text.strip()
     
     current_price = soup.find('span', attrs={'class': 'priceToPay'}).find('span', attrs={'class':'a-offscreen'}).text
+    current_price = current_price.replace(',', '')
     curr_price_decimal = float(current_price[1:])
 
     mrp = soup.find('span', attrs={'class': 'a-text-price'}).find('span', attrs={'class':'a-offscreen'}).text
+    mrp = current_price.replace(',', '')
     mrp_decimal = float(mrp[1:])
 
-    return 
-    {
+    asin = soup.find('table', attrs={'id': 'productDetails_detailBullets_sections1'}).find('td', attrs={'class': 'prodDetAttrValue'}).text
+
+    return {
+        'asin': asin,
         'title': title,
         'current_price': curr_price_decimal,
         'list_price': mrp_decimal
-    }
+        }
 
+def get_url(request):
+    url = 'https://www.amazon.in/dp/B09NVPSCQT'
+
+    data = get_product_details(url)
+
+    return HttpResponse(json.dumps(data))
 
