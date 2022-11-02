6ab56620369477ff22c2cff842432b0e3ea4ef5c
prateekb1912 <prateekb1912@gmail.com>
2022-11-02 16:49:36 +0530

    Install requests and bs4 for parsing

--- pricetracker/tracker/models.py
+++ pricetracker/tracker/models.py
@@ -8,3 +8,6 @@ class Product(models.Model):
     list_price = models.DecimalField(max_digits=10, decimal_places=2)
     sell_price = models.DecimalField(max_digits=10, decimal_places=2)
     date_added = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
--- pricetracker/tracker/views.py
+++ pricetracker/tracker/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
+import requests
+from bs4 import BeautifulSoup
+
 # Create your views here.
+def get_product_details(url):
+    headers = {
+         'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'
+    }
+    resp = requests.get(url, headers=headers)
+    page = resp.content
+    
+        
