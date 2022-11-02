b13306c4576e76c46a0a9f69fea19c6331fbaee4
prateekb1912 <prateekb1912@gmail.com>
2022-11-02 15:31:37 +0530

    Add product model

--- pricetracker/tracker/models.py
+++ pricetracker/tracker/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=256)
+    url = models.URLField(max_length=512)
+    slug = models.SlugField()
+    list_price = models.DecimalField(max_digits=10, decimal_places=2)
+    sell_price = models.DecimalField(max_digits=10, decimal_places=2)
+    date_added = models.DateField(auto_now_add=True)
