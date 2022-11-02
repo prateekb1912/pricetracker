f19e277ba1c272fe484c64cec098ae4687a118d1
prateekb1912 <prateekb1912@gmail.com>
2022-11-02 17:22:27 +0530

    Define view to scrape product page

--- pricetracker/tracker/views.py
+++ pricetracker/tracker/views.py
@@ -9,6 +9,24 @@ def get_product_details(url):
          'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'
     }
     resp = requests.get(url, headers=headers)
-    page = resp.content
+    page = resp.text
+
+    soup = BeautifulSoup(page, 'html.parser')
+
+    # Get the product details from the product page
+    title = soup.find('h1', attrs={'id':'title'}).text.strip()
     
-        
+    current_price = soup.find('span', attrs={'class': 'priceToPay'}).find('span', attrs={'class':'a-offscreen'}).text
+    curr_price_decimal = float(current_price[1:])
+
+    mrp = soup.find('span', attrs={'class': 'a-text-price'}).find('span', attrs={'class':'a-offscreen'}).text
+    mrp_decimal = float(mrp[1:])
+
+    return 
+    {
+        'title': title,
+        'current_price': curr_price_decimal,
+        'list_price': mrp_decimal
+    }
+
+
