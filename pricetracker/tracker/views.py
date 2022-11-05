6d65225431d2ad80d0efd6d1f4c371ef822bb14a
prateekb1912 <prateekb1912@gmail.com>
2022-11-05 16:27:06 +0530

    Display JSON data from input URL

--- pricetracker/tracker/views.py
+++ pricetracker/tracker/views.py
@@ -15,25 +15,30 @@ def get_product_details(url):
     page = resp.text
 
     soup = BeautifulSoup(page, 'html.parser')
+    try:
 
-    # Get the product details from the product page
-    title = soup.find('h1', attrs={'id':'title'}).text.strip()
-    
-    current_price = soup.find('span', attrs={'class': 'priceToPay'}).find('span', attrs={'class':'a-offscreen'}).text
-    current_price = current_price.replace(',', '')
-    curr_price_decimal = float(current_price[1:])
+        # Get the product details from the product page
+        title = soup.find('h1', attrs={'id':'title'}).text.strip()
+        
+        current_price = soup.find('span', attrs={'class': 'priceToPay'}).find('span', attrs={'class':'a-offscreen'}).text
+        current_price = current_price.replace(',', '')
+        curr_price_decimal = float(current_price[1:])
 
-    mrp = soup.find('span', attrs={'class': 'a-text-price'}).find('span', attrs={'class':'a-offscreen'}).text
-    mrp = current_price.replace(',', '')
-    mrp_decimal = float(mrp[1:])
+        mrp = soup.find('span', attrs={'class': 'a-text-price'}).find('span', attrs={'class':'a-offscreen'}).text
+        mrp = current_price.replace(',', '')
+        mrp_decimal = float(mrp[1:])
 
-    asin = soup.find('table', attrs={'id': 'productDetails_detailBullets_sections1'}).find('td', attrs={'class': 'prodDetAttrValue'}).text
+        asin = soup.find('table', attrs={'id': 'productDetails_detailBullets_sections1'}).find('td', attrs={'class': 'prodDetAttrValue'}).text
 
-    return {
-        'asin': asin,
-        'title': title,
-        'current_price': curr_price_decimal,
-        'list_price': mrp_decimal
+        return {
+            'asin': asin,
+            'title': title,
+            'current_price': curr_price_decimal,
+            'list_price': mrp_decimal
+            }
+    except Exception:
+        return {
+            'error': 'Invalid URL!'
         }
 
 def get_url(request):
@@ -43,10 +48,13 @@ def get_url(request):
 
     return HttpResponse(json.dumps(data, indent=4), content_type='application/json')
 
-@require_http_methods(["GET", "POST"])
 def index(request):
-    print(request.method == 'POST')
     if request.method == 'POST':
-        print(request.GET)
+        post_data = request.POST
+        url = post_data['inputURL']
+
+        product_data = get_product_details(url)
+        return HttpResponse(json.dumps(product_data, indent=4))
+
 
     return render(request, 'index.html')
