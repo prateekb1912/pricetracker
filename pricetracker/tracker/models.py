9e981c3a36b0417a2095b2ea0be57e8305a68b16
prateekb1912 <prateekb1912@gmail.com>
2022-11-08 21:48:01 +0530

    Add image field to products

--- pricetracker/tracker/models.py
+++ pricetracker/tracker/models.py
@@ -5,6 +5,7 @@ class Product(models.Model):
     asin = models.CharField(max_length=20, unique = True, default='ASIN000')
     title = models.CharField(max_length=256)
     url = models.URLField(max_length=512)
+    product_image = models.ImageField(upload_to='imgs/', null=True)
     list_price = models.DecimalField(max_digits=10, decimal_places=2)
     sell_price = models.DecimalField(max_digits=10, decimal_places=2)
     date_added = models.DateField(auto_now_add=True)
