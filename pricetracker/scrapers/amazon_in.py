656d17d19bf32c11ad0ed3641d62eaac258076f6
prateekb1912 <prateekb1912@gmail.com>
2022-11-17 23:04:08 +0530

    Move to scraper function to its own folder

---
+++ pricetracker/scrapers/amazon_in.py
@@ -0,0 +1,52 @@
+import requests
+import time
+
+from bs4 import BeautifulSoup
+import logging
+
+logger = logging.getLogger(__name__)
+
+def get_product_details(url):
+    headers = {
+         'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'
+    }
+    
+    try:
+        resp = requests.get(url, headers=headers)
+        time.sleep(2)
+
+        page = resp.text
+
+        soup = BeautifulSoup(page, 'html.parser')
+
+        title = soup.find('span', attrs={'id':'productTitle'}).text.strip()
+        logger.warning(f"Product: {title}")
+        
+        current_price = soup.find('span', attrs={'class': 'priceToPay'}).find('span', attrs={'class':'a-offscreen'}).text
+        current_price = current_price.replace(',', '')
+        curr_price_decimal = float(current_price[1:])
+        logger.warning(f"Available @ {current_price}")
+
+        mrp = soup.find('span', attrs={'class': 'basisPrice'}).find('span', attrs={'class':'a-offscreen'}).text
+        mrp = mrp.replace(',', '')
+        mrp_decimal = float(mrp[1:])
+        logger.warning(f"MRP: {mrp}")
+
+
+        asin = soup.find('table', attrs={'id': 'productDetails_detailBullets_sections1'}).find('td', attrs={'class': 'prodDetAttrValue'}).text
+        logger.warning(f"ASIN: {asin}")
+
+        landingImg = soup.find('div', attrs={'id': 'main-image-container'}).find('img').get('src')
+        logger.warning(f"Image has been scraped: {landingImg}")
+
+        return {
+            'title': title,
+            'listPrice': mrp_decimal,
+            'currentPrice': curr_price_decimal,
+            'imageURL': landingImg
+            }
+
+    except Exception:
+        return {
+            'error': 'Invalid URL!'
+        }
--- pricetracker/tracker/views.py
+++ pricetracker/tracker/views.py
@@ -1,70 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-import requests
-import json
-import time
 import logging
 
-from bs4 import BeautifulSoup
-
 from .models import Product
+from scrapers.amazon_in import get_product_details
 
 logger = logging.getLogger(__name__)
-
-# Create your views here.
-def get_product_details(request, url):
-    headers = {
-         'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'
-    }
-    
-    try:
-        resp = requests.get(url, headers=headers)
-        time.sleep(2)
-
-        page = resp.text
-
-        soup = BeautifulSoup(page, 'html.parser')
-        # Get the product details from the product page
-
-        title = soup.find('span', attrs={'id':'productTitle'}).text.strip()
-        logger.warning(f"Product: {title}")
-        
-        current_price = soup.find('span', attrs={'class': 'priceToPay'}).find('span', attrs={'class':'a-offscreen'}).text
-        current_price = current_price.replace(',', '')
-        curr_price_decimal = float(current_price[1:])
-        logger.warning(f"Available @ {current_price}")
-
-        mrp = soup.find('span', attrs={'class': 'basisPrice'}).find('span', attrs={'class':'a-offscreen'}).text
-        mrp = mrp.replace(',', '')
-        mrp_decimal = float(mrp[1:])
-        logger.warning(f"MRP: {mrp}")
-
-
-        asin = soup.find('table', attrs={'id': 'productDetails_detailBullets_sections1'}).find('td', attrs={'class': 'prodDetAttrValue'}).text
-        logger.warning(f"ASIN: {asin}")
-
-        landingImg = soup.find('div', attrs={'id': 'main-image-container'}).find('img').get('src')
-        logger.warning(f"Image has been scraped: {landingImg}")
-
-        return {
-            'title': title,
-            'listPrice': mrp_decimal,
-            'currentPrice': curr_price_decimal,
-            'imageURL': landingImg
-            }
-
-    except Exception:
-        return {
-            'error': 'Invalid URL!'
-        }
 
 def index(request):
     if request.method == 'POST':
         post_data = request.POST
         url = post_data['inputURL']
         
-        product_data = get_product_details(request, url)
+        product_data = get_product_details(url)
         return render(request, 'product_focus.html', context=product_data)
 
     return render(request, 'index.html')
