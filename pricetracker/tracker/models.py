904a040b7f46095e8b74cde91e5c2335a441001a
prateekb1912 <prateekb1912@gmail.com>
2022-11-28 23:44:28 +0530

    Change date field to timestamp field

--- pricetracker/tracker/models.py
+++ pricetracker/tracker/models.py
@@ -8,7 +8,7 @@ class Product(models.Model):
     product_image = models.URLField(max_length=512)
     list_price = models.DecimalField(max_digits=10, decimal_places=2)
     sell_price = models.DecimalField(max_digits=10, decimal_places=2)
-    date_added = models.DateField(auto_now_add=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f'{self.title} @ {self.sell_price}'
