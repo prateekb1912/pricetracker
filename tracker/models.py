6685ea57d54295601582020373fde65168097283
prateekb1912 <prateekb1912@gmail.com>
2022-12-12 20:41:52 +0530

    Add modified column in product

--- tracker/models.py
+++ tracker/models.py
@@ -7,7 +7,8 @@ class Product(models.Model):
     url = models.URLField(max_length=512, null=True)
     product_image = models.URLField(max_length=512)
     sell_price = models.DecimalField(max_digits=10, decimal_places=2)
-    timestamp = models.DateTimeField(auto_now_add=True)
+    added_at = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f'{self.title} available currently @ {self.sell_price} added on {self.timestamp} '
