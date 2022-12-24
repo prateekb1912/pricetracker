0f597a9bea7d26b142e6430f02860a89b578ef4d
prateekb1912 <prateekb1912@gmail.com>
2022-12-24 20:32:12 +0530

    Add environment variables to store secret keys

--- app/pricetracker/settings.py
+++ app/pricetracker/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-p0z@3*12mc_%l!b5f2y6)ge)iy9-f2su813w#w2mzpra+0h2el'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get("DEBUG", default=0))
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
