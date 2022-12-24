498e552111ecd9b1fd9174837a0ce86cd17251b7
prateekb1912 <prateekb1912@gmail.com>
2022-12-24 21:42:01 +0530

    Add postgres credentials in env file

--- app/pricetracker/settings.py
+++ app/pricetracker/settings.py
@@ -104,12 +104,12 @@ WSGI_APPLICATION = 'pricetracker.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'pricetracker',
-        'USER': 'patrick',
-        'PASSWORD': 'patrick19135',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': os.environ.get("SQL_ENGINE", 'django.db.backebds.sqlite3'),
+        'NAME': os.environ.get("SQL_DATABASE", BASE_DIR/'db.sqlite3'),
+        'USER': os.environ.get("SQL_USER", 'user'),
+        'PASSWORD': os.environ.get("SQL_PASSWORD", 'password'),
+        'HOST': os.environ.get("SQL_HOST", 'localhost'),
+        'PORT': os.environ.get("SQL_PORT", '5432'),
     }
 }
 
