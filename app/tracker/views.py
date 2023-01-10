79e5fbb31b6704542971c6bb932f1fdd5ea19b2c
prateekb1912 <prateekb1912@gmail.com>
2023-01-10 22:54:17 +0530

    Require login when accessing views

--- app/tracker/views.py
+++ app/tracker/views.py
@@ -1,5 +1,7 @@
 import logging
 
+from django.contrib import messages
+from django.contrib.auth import login, logout
 from django.contrib.auth.hashers import make_password
 from django.contrib.auth.decorators import login_required
 from django.core.exceptions import ObjectDoesNotExist
@@ -35,24 +37,27 @@ def register_user(request):
     form = UserCreationForm()
 
     if request.method == 'POST':
-        logger.warning(request.POST)
+        data = request.POST
+        logger.warning(data)
 
         data = request.POST.copy()
         data._mutable = True
-
         data['password'] = make_password(request.POST['password'])
 
         form = UserCreationForm(data)
 
         if form.is_valid():
-            form.save()
-        
-        return redirect('index')
+            user = form.save()
+            login(request, user)
+            messages.success(request, 'Register success')
+            
+            return redirect('index')
 
     context = {'form': form}
 
     return render(request, 'register.html', context)
 
+@login_required(login_url='/register/', redirect_field_name='')
 def view_product(request, asin):
     try:
         product = Product.objects.filter(asin__contains=asin)
@@ -61,12 +66,13 @@ def view_product(request, asin):
     except ObjectDoesNotExist:
         return HttpResponseBadRequest({'error': 'Wrong ASIN'})
 
-
+@login_required(login_url='/register/', redirect_field_name='')
 def list_all_products(request):
     products_list = Product.objects.order_by('-added_at')
 
     return render(request, template_name='products_list.html', context={'list': products_list})
 
+@login_required(login_url='/register/', redirect_field_name='')
 def delete_product(request, asin):
     product = Product.objects.get(asin=asin)
     if request.method == 'POST':
